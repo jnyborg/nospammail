671521afaae43748b4f839e4e099bbe98bee31d4
Joachim Christiansen Nyborg <jnyborg@outlook.com>
2017-04-01 13:30:02 +0200

    added db connections

--- nospammail/settings.py
+++ nospammail/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'nospammail.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'nospammail',
+        'USER': 'nospammail',
+        'PASSWORD': os.environ.get('NOSPAMMAIL_PW', False),
+        'HOST': os.environ.get('NOSPAMMAIL_HOST', False),
+        'PORT': '5432',
     }
 }
 
