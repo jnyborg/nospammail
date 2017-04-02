88c694ada055a66a6c241c808f69ab3fa5cc3650
Alexander Qvist-Hellum <Alexander@svejbaekgaard.dk>
2017-04-02 02:07:08 +0200

    First attempt at real forwarding

--- backend/email_processor.py
+++ backend/email_processor.py
@@ -1,5 +1,7 @@
 import json
 import smtplib
+import psycopg2
+import os
 
 def processEmail(message):
     #print("message: %s" % message)
@@ -22,16 +24,40 @@ def processEmail(message):
     subject = m['commonHeaders']['subject']
     print("subject: %s" % subject)
 
-    actualEmail = "jnyborg@outlook.com"
-
-    sender = destinations[0]
-    receivers = [ actualEmail ]
-    print(m)
-    msg = d['content']
-
     try:
-        smtpObj = smtplib.SMTP('localhost', 25)
-        smtpObj.sendmail(sender, receivers, msg)
-        print("Successfully sent email!")
-    except smtplib.SMTPException as e:
-        print("ERROR: %s: %s" % e.errno, e.strerror)
+        conn = psycopg2.connect("dbname='nospammail' " \
+                                "user='nospammail' " \
+                                "host={}" \
+                                "password={}".format(
+                                        os.environ.get('NOSPAMMAIL_HOST', False),
+                                        os.environ.get('NOSPAMMAIL_PW', False)))
+
+        cur = conn.cursor()
+        cur.execute("select g.enabled as enabled, u.email as email from settings_console_generatedemail as g left join auth_user as u on g.id = u.user_id where g.email={};".format(destinations[0]))
+        rows = cur.fetchall()
+
+        if len(rows) <= 0:
+            print("Error, {} has no owner!".format(destinations[0]))
+            return
+        else:
+            if rows[0]['enabled']:
+                sender = destinations[0]
+                receivers = [rows[0]['email']]
+                print(m)
+                msg = d['content']
+
+                try:
+                    smtpObj = smtplib.SMTP('localhost', 25)
+                    smtpObj.sendmail(sender, receivers, msg)
+                    print("Successfully sent email!")
+                except smtplib.SMTPException as e:
+                    print("ERROR: %s: %s" % e.errno, e.strerror)
+            else:
+                print("Email linked to {} has disabled forwarding.".format(destinations[0]))
+    except:
+        print("Unable to connect to database!")
+
+
+
+
+
