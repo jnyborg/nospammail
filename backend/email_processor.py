76b9bb9cddcc0e1187fd90522264d49ec7c3b850
Alexander Qvist-Hellum <Alexander@svejbaekgaard.dk>
2017-05-12 15:05:43 +0200

    Added multi threading for email processing
    Added better logging for errors when being unable to send email
    Added filtering in case emails are sent with multiple receivers

--- backend/email_processor.py
+++ backend/email_processor.py
@@ -4,25 +4,21 @@ import psycopg2
 import os
 
 def processEmail(message):
-    #print("message: %s" % message)
+
     d = json.loads(message['Message'])
-    #print("d: %s" % d)
+
     m = d['mail']
-    #print("m: %s" % m)
-    source = m['source']
-    #print("source: %s" % source)
-    destinations = m['destination']
-    destinationsPrefix = map(lambda x: x.split('@')[0], destinations)
-    #print("destinations:")
+
+    sender = m['source']
+    destinationsTemp = m['destination']
+
+    destinations = list(filter(lambda x: "@nospammail.org" in x, destinationsTemp)) # Remove any emails that aren't sent to the nospammail.org domain
+
+    forwarder = destinations[0]
+
+    print("Should forward email to:")
     for des in destinations:
         print(des)
-
-    #print("destinationsPre:")
-    for des in destinationsPrefix:
-        print(des)
-
-    subject = m['commonHeaders']['subject']
-    #print("subject: %s" % subject)
 
     try:
         conn = psycopg2.connect("dbname='nospammail' " \
@@ -31,32 +27,34 @@ def processEmail(message):
                                 "password='{}'".format(
                                         os.environ.get('NOSPAMMAIL_HOST', False),
                                         os.environ.get('NOSPAMMAIL_PW', False)))
-
+        
         cur = conn.cursor()
-        cur.execute("select g.enabled as enabled, u.email as email from settings_console_generatedemail as g left join auth_user as u on g.user_id = u.id where g.email='{}';".format(destinations[0]))
+        cur.execute("select g.enabled as enabled, u.email as email from settings_console_generatedemail as g left join auth_user as u on g.user_id = u.id where g.email='{}';".format(forwarder))
         rows = cur.fetchall()
 
         if len(rows) <= 0:
-            print("Error, {} has no owner!".format(destinations[0]))
+            print("Error, {} has no owner! Sender was {}".format(destinations[0], sender))
             return
         else:
             if rows[0][0]:
-                sender = destinations[0]
+                forwarder = destinations[0]
                 receivers = [rows[0][1]]
                 msg = d['content']
 
                 try:
                     smtpObj = smtplib.SMTP('localhost', 25)
-                    smtpObj.sendmail(sender, receivers, msg)
+                    smtpObj.sendmail(forwarder, receivers, msg)
                     print("Successfully sent email!")
                 except smtplib.SMTPException as e:
                     print("ERROR: %s: %s" % e.errno, e.strerror)
             else:
                 print("Email linked to {} has disabled forwarding.".format(destinations[0]))
     except Exception as e:
-        print("Unable to connect to DB: %s: %s" % e.errno, e.strerror)
-
-
-
-
-
+        if hasattr(e, 'reason'):
+            print("Unable to connect to DB, error reason: {}".format(e.reason))
+        elif hasattr(e, 'code'):  # <--
+            print("Unable to connect to DB, error code: {}".format(e.code))
+        elif hasattr(e, 'errno'):
+            print("Unable to connect to DB: {}: {}".format(e.errno, e.strerror))
+        else:
+            print("Unable to connect to DB: unspecified error: {}".format(str(e)))
--- backend/sqs_listener.py
+++ backend/sqs_listener.py
@@ -1,8 +1,25 @@
 import boto3
 import json
+from multiprocessing.dummy import Pool as ThreadPool
+import os
+import sys
+import threading
+
 from email_processor import processEmail
 
 def ListenForMails():
+    THREAD_COUNT = os.cpu_count() - 1  # Leave dedicated thread for listener
+
+    try:
+        threadArg = int(sys.argv[1]) # First argument overrides thread pool size
+
+        if threadArg > 0:
+            THREAD_COUNT = threadArg
+    except IndexError: # No thread count was specified
+        pass
+
+    pool = ThreadPool(THREAD_COUNT)
+
     client = boto3.setup_default_session(region_name='eu-west-1')
 
     sqs = boto3.resource('sqs')
@@ -13,20 +30,17 @@ def ListenForMails():
     print("Current delay is %s" % queue.attributes.get('DelaySeconds'))
 
     while True:
-        for message in queue.receive_messages(MessageAttributeNames=['Author']):
-            print("There's a new message!")
-            author_text = ""
-            if message.message_attributes is not None:
-                author_name = message.message_attributes.get('Author').get('StringValue')
-                if author_name:
-                    author_text = " ({0})".format(author_name)
+        pool.map(ReceiveEmail, queue.receive_messages(MessageAttributeNames=['Author']))
 
-            processEmail(json.loads(message.body))
+    pool.close()
+    pool.join()
 
-            message.delete()
-            #break Used for the else statement
-        #else:
-            #print("Queue is empty!")
+def ReceiveEmail(message):
+    print("There's a new message! Thread {} is handling it.".format(threading.get_ident()))
+
+    processEmail(json.loads(message.body))
+
+    message.delete()
 
 if __name__ == "__main__":
     ListenForMails()
